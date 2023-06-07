f64c3e155de55b62f924d69ddeb7d3dbd86e126b
Fabricio-Roosevelt <fabriciorms@hotmail.com>
2023-06-07 15:17:37 -0300

    implementacao finByDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,6 @@
 package application;
 
-import java.util.Date;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -16,7 +16,13 @@ public class Program {
 		System.out.println(" **** TEST 1: seller findById **** ");
 		Seller seller = sellerDao.finById(3);
 		System.out.println(seller);
-
+		System.out.println();
+		
+		System.out.println(" **** TEST 2: seller findByDEpartmente **** ");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
-
 }
